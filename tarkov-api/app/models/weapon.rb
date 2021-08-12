079e64c66090e3e645563fd7c4024d95e0762e58
jackcornblum <jcornblum@gmail.com>
2021-08-12 17:19:53 -0400

    best handguard method done

--- tarkov-api/app/models/weapon.rb
+++ tarkov-api/app/models/weapon.rb
@@ -34,7 +34,11 @@ class Weapon < ApplicationRecord
     def best_receiver_recoil
         if self.receivers.count > 0
             receivers = self.receivers
-            best_receiver = receivers.max_by{|receiver| receiver[:recoil]}
+            # best_receiver = receivers.max_by{|receiver| receiver[:recoil]}
+            recoils = receivers.map{|r| r[:recoil]}
+            max = recoils.max()
+            best_receivers = receivers.select{|r| r[:recoil] == max}
+            best_receiver = best_receivers.max_by{|r| r[:ergonomics]}
             best_receiver
         else
             "No receivers"
@@ -51,10 +55,20 @@ class Weapon < ApplicationRecord
         else
             if self.receivers.count > 0
                 receivers = self.receivers
+
                 best_receiver = receivers.max_by{|receiver| receiver[:recoil]}
                 handguards = best_receiver.handguards
-                byebug
+                if handguards.length > 0
+                    recoils = handguards.map{|h| h[:recoil]}
+                    max = recoils.max()
+                    best_guards = handguards.select{|h| h[:recoil] == max}
+                    best_handguard = best_guards.max_by{|g| g[:ergonomics]}
+                    best_handguard
+                else
+                    "No handguards"
+                end
             else
+                "No handguards"
             end
             
         end
