f78a3364b87a2b562d7c59d409bbef60d4d8c24e
jackcornblum <jcornblum@gmail.com>
2021-08-13 16:29:43 -0400

    weapon cards displayed with correct img src

--- tarkov-helper-frontend/src/Components/Gun.js
+++ tarkov-helper-frontend/src/Components/Gun.js
@@ -5,9 +5,15 @@ import { useEffect, useState } from "react"
 import {useHistory} from 'react-router-dom'
 
 function Gun({name, image, caliber, gun_id}) {
+    let goodImage = image.split('/revision')[0]
+    console.log(goodImage)
     return(
-        <>
-        </>
+        <Card className="gunCard">
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Img variant="bottom" src={goodImage} />
+            </Card.Body>
+        </Card>
     )
 }
 
--- tarkov-helper-frontend/src/Components/Weapons.js
+++ tarkov-helper-frontend/src/Components/Weapons.js
@@ -1,13 +1,22 @@
 import Gun from './Gun.js'
+import { useEffect, useState } from "react"
+import {Container, Row, Button} from 'react-bootstrap'
+import Col from 'react-bootstrap/Col'
 
 function Weapons({weapons}) {
 
+    const [allWeapons, setAllWeapons] = useState(true)
+    
     let assaultRifles = weapons.filter(gun => gun.weapon_type === 'Assault rifle')
     let assaultCarbines = weapons.filter(gun => gun.weapon_type === 'Assault carbine')
     let submachineGuns = weapons.filter(gun => gun.weapon_type === 'Submachine gun')
     let shotguns = weapons.filter(gun => gun.weapon_type === 'Shotgun')
     let marksmanRifles = weapons.filter(gun => gun.weapon_type === 'Marksman rifle')
     let sniperRifles = weapons.filter(gun => gun.weapon_type === 'Sniper rifle')
+
+    let renderWeapons = weapons.map(gun => {
+        return <Gun name={gun.name} image={gun.image} caliber={gun.caliber} gun_id={gun.id} key={gun.id} />
+    })
 
     let renderAssaultRifles = assaultRifles.map(gun => {
         return <Gun name={gun.name} image={gun.image} caliber={gun.caliber} gun_id={gun.id} key={gun.id} />
@@ -17,7 +26,12 @@ function Weapons({weapons}) {
     return(
         <div className="weapons-container">
             <h2>Weapons</h2>
-            <button onClick={e => console.log(submachineGuns)}>d</button>
+            {allWeapons ? <h3>All Weapons</h3> : null}
+            <Container fluid="md">
+                <Row md={3}>
+                {allWeapons ? renderWeapons : null}
+                </Row>
+            </Container>
 
         </div>
     )
